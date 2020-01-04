6ae1bc1018051dfe047005c1941ee5b19112181d
Vamsi <bvamsi482@gmail.com>
2020-01-04 20:10:31 +0530

    Removed tfvars

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 
 provider "azurerm" {
   version  = "1.39"
-  client_id = "${var.client_id}"
-  client_secret = "${var.client_secret}"
-  subscription_id = "${var.subscription_id}"
-  tenant_id       = "${var.tenant_id}"
+  client_id = var.client_id
+  client_secret = var.client_secret
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_resource_group" "rg" {
